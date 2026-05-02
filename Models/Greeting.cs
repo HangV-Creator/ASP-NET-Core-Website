61fa5b356d2e9090dda9d4c90206e2885069844b
Vangxue Hang <hangvangxue@gmail.com>
2026-05-01 23:44:41 -0400

    Create Greeting.cs

---
+++ Models/Greeting.cs
@@ -0,0 +1,7 @@
+namespace WebsiteProject.Models {
+    public class Greeting {
+        public string Message { get; set; }
+        public string Timestamp { get; set; }
+        public bool IsDaytime { get; set; }
+    }
+}
